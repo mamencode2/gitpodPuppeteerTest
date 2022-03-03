fb90043cacd040ef015a36699efb3784981170f1
mamencode2 <mamencode2@gmail.com>
2022-03-03 19:34:00 +0000

    cd

--- example.js
+++ example.js
@@ -5,7 +5,11 @@ const iPhone = puppeteer.devices['iPhone 6'];
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.emulate(iPhone);
-  await page.goto('https://www.google.com');
+  await page.goto('https://app.tutoraround.com/tutors-near-me');
+  await page.setDefaultTimeout(0)
   await page.screenshot({ path: 'screenshot.png' });
   await browser.close();
 })();
+
+
+//https://app.tutoraround.com/tutors-near-me
