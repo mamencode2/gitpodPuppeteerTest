ea026aa798ea1da3d9b80d85084e62b8e29bd8be
mamencode2 <mamencode2@gmail.com>
2022-03-04 21:39:20 +0000

    cd

--- example.js
+++ example.js
@@ -1,15 +1,53 @@
+const fs = require('fs');
 const puppeteer = require('puppeteer');
-const iPhone = puppeteer.devices['iPhone 6'];
+//const iPhone = puppeteer.devices['iPhone 6'];
+//const fs = require("fs/promises")
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.emulate(iPhone);
-  await page.goto('https:tutoraround.com');
-  await page.setDefaultTimeout(0)
-  await page.screenshot({ path: 'screenshot.png' });
-  await browser.close();
+  
+  
+
+
+
+
+
+  const browser = await puppeteer.launch()
+const page = await browser.newPage()
+const context = browser.defaultBrowserContext()
+await context.overridePermissions("https://app.tutoraround.com/tutors-near-me", ['geolocation'])
+await page.setGeolocation({latitude:90, longitude:20})
+    //set the location
+await page.goto("https://app.tutoraround.com/tutors-near-me", {
+    waitUntil: "networkidle0"
+})
+
+
+const photos = await page.$$eval(".profile-image ", imgs => {
+    return imgs.map(x => x.src)
+   })
+
+//console.log(...photos)
+const lsp = photos.map((index,photo)=>{
+    return {
+        id: photo,
+        imgUrl: index.split("?")[0]
+    }
+})
+console.log(lsp)
+fs.writeFile('./datas.json', JSON.stringify(lsp), err => err ? console.log(err): null);
+
+  //console.log(photos[0].split("?")[0])
+
+const contents = await page.$$eval("ion-grid", nodes => {
+    return nodes.map(node => {
+        const nameI = node.querySelector("name-details")
+    })
+
+    return {nameI}
+})
+
+//console.log(contents)
+
+
+await browser.close();
 })();
-
-
-//https://app.tutoraround.com/tutors-near-me
