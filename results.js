59bed3934a4afd69f06fabe2a24f1d79d38d3ee2
mamencode2 <mamencode2@gmail.com>
2022-03-14 15:32:26 +0000

    cx

---
+++ results.js
@@ -0,0 +1,128 @@
+const results = [
+
+    {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "iPhone Earbuds Wired Lightning Headphones [Apple MFi Certified] Stereo Noise Canceling Isolating in Ear Headset with Built-in Microphone&Volume Control Compatible with iPhone 13 12 SE 11 X 8 7-All IOS",
+        "$22.00": "$7.99",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/41-HyN9AxcL._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "New X7K8W GEN14 3.5\" HDD Tray Caddy for POWEREDGE Server R740 R740xd R7415 R940 R640 R6415",
+        "$22.00": "$9.89",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/61EZC7-ShpL._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "SZMYX 3.5 inch Drive Caddy with 2.5 inch HDD Adapter Compatible for 14th Dell PowerEdge Servers R240 R340 R440 R540 R640 R6415 R740 R740xd R740xd2 R6415 R7415 R7425 X7K8W",
+        "$22.00": "Null",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/61RIA8F1XNL._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "Pulse Chip Mod ACE V3 Chip with Improved Code Good Electric Component and Clear Circuit Layout Supports for X360 ACE V3",
+        "$22.00": "$11.59",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/61TmuxweYRL._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "XIAOGE Elite Strap for Oculus Quest 2 with Head Cushion & Mobile Power Fixing Bracket",
+        "$22.00": "Adjustable Comfortable Protective Head Strap Compatibly Multiple Sizes Powerbank",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "$19.99"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "New X7K8W GEN14 3.5\" HDD Tray Caddy for POWEREDGE Server R440 R740 R740xd R7415 R940 R640 R6415",
+        "$22.00": "$10.99",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/61G-nB41mML._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "Resilient Facial Cover Pad and Silicone Light Blockers Accessories for Oculus Quest 2",
+        "$22.00": "Breathable Sweat Foam Widen Face - Vent Soft Interface Bracket with Anti-Leakage Nose Pad",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "$19.97"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "Chip for Bitmain Antminer S9",
+        "$22.00": "$14.00",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/21kcRsipy8L._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "Panduit S8222B9",
+        "$22.00": "$13.69",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/71KeWzf6vhL._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "Lenovo DCG X8 X8 X8ml2 Pcie Fh Riser1 Kit",
+        "$22.00": "$17.78",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/41VcF7MY0KL._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "HP SAS Internal Cable Kit - for ProLiant DL380 Gen9 785989-B21",
+        "$22.00": "$8.83",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/61KxDjOA7-L._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "Pulse Chip Mod ACE V3 Chip with Improved Code Good Electric Component and Clear Circuit Layout Supports for X360 ACE V3",
+        "$22.00": "$11.59",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/61TmuxweYRL._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "Dell Part # 0J0686",
+        "$22.00": "",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "Null"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "iStarUSA DD-300-SEA-RED 3U Compact Stylish Rack Mount Chassis with Sea Bezel - Red",
+        "$22.00": "$8.84",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/31kIxOj9X4L._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "HP Ribbon Cable 80WAY for PROLIANT",
+        "$22.00": "Null",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/71Xe6lVx0RL._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "412202-001:DL360 MULTIBAY MEDIA",
+        "$22.00": "Null",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/61zVmbXDL2L._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "IBM 4849KDF Windows Small Business Server 2011 (CD-ROM)",
+        "$22.00": "$22.31",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/71nL4XUcG6L._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "HP 452698-B21 PCI-X Riser Kit FORDL160G5/DL165G5 Serial Port Card",
+        "$22.00": "$14.91",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/41foOQuFW2S._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "Zoom 36.6K Non-Upgradeable External Faxmodem for PC 2836-00-00",
+        "$22.00": "$18.18",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/91J-W9GCdJL._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "ARM",
+        "$22.00": "CAM-WIDE-P1-65 GRAM W/SET SCREW",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "$8.19"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "Arctic Cat 0746-694 ARM",
+        "$22.00": "CAM-WIDE-P1-63 GRAM W/SET SCREW",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "3 Pack"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "Panduit S6219BF",
+        "$22.00": "$13.37",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/51PAhu22CvL._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "Panduit N8522BYT",
+        "$22.00": "$17.16",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "https://m.media-amazon.com/images/I/41ogglUD7aL._AC_UL320_.jpg"
+      },
+      {
+        "Pace VR Cover for Oculus Quest 2 - Sweat Absorbent (2 Pack)": "Monoprice Blank Dcor Insert - White for Home",
+        "$22.00": "Office",
+        "https://m.media-amazon.com/images/I/815CPLi5SML._AC_UL320_.jpg": "Personal Install"
+      }
+
+
+]
+
+
+export default results
